c0b59e83dae7c6516b96087b304dc5b1749ce7fd
Tim (robske_110) <robske110@users.noreply.github.com>
2021-02-23 16:30:31 +0100

    Cache carPicture.php

--- public/carPicture.php
+++ public/carPicture.php
@@ -1,4 +1,8 @@
 <?php
+session_cache_limiter('none');
+const YEAR = 60*60*24*365;
+header("Cache-control: max-age=".YEAR);
+header("Expires: ".(new DateTime("@".(time()+YEAR)))->format(DateTime::RFC1123));
 define("ALLOW_KEY_AUTHENTICATION", true);
 require "login/loginCheck.php";
 
