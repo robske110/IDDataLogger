858aa44f7e53170d1c598ed08f317d6e566c3835
Tim (robske_110) <robske110@users.noreply.github.com>
2021-02-27 08:52:00 +0100

    Handle carPicture fetch errors gracefully and continue
    Fixes #2

--- src/vwid/CarPictureHandler.php
+++ src/vwid/CarPictureHandler.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace robske_110\vwid;
 
+use robske_110\utils\ErrorUtils;
 use robske_110\utils\Logger;
 use robske_110\utils\QueryCreationHelper;
 use robske_110\vwid\api\LoginInformation;
 use robske_110\vwid\api\WebsiteAPI;
+use RuntimeException;
 
 class CarPictureHandler{
 	const PICTURE_LOCATION = BASE_DIR."data/carPic.png";
@@ -18,7 +20,16 @@ class CarPictureHandler{
 		@mkdir(BASE_DIR."data/");
 		if(!file_exists(BASE_DIR."data/carPic.png")){
 			Logger::log("Fetching carPicture (this will take a while...)");
-			$this->fetchCarPicture();
+			try{
+				$this->fetchCarPicture();
+			}catch(RuntimeException $e){
+				ErrorUtils::logException($e);
+				Logger::warning("Failed to automatically fetch the carPicture! You can substitute data/carPic.png manually.");
+				$im = imagecreate(100, 100);
+				imagecolorallocate($im, 255, 0, 0);
+				imagepng($im, self::PICTURE_LOCATION, 9, PNG_NO_FILTER);
+			}
+			
 		}
 		$this->main->getDB()->query(
 			"INSERT INTO carPictures(pictureID, carPicture) VALUES('default', '".base64_encode(file_get_contents(self::PICTURE_LOCATION))."') ".
@@ -42,19 +53,22 @@ class CarPictureHandler{
 			}
 		}
 		if(!isset($imageUrl)){
-			throw new \RuntimeException("Unable to fetch a car picture");
+			throw new RuntimeException("Unable to fetch a car picture: Could not find uri.");
 		}
 		file_put_contents(self::PICTURE_LOCATION, file_get_contents($imageUrl));
 		
 		$im = imagecreatefrompng(self::PICTURE_LOCATION);
+		if($im === false){
+			throw new RuntimeException("Failed to fetch car picture: Failed to download picture. (Check for up to date CA definitions!)");
+		}
 		imagealphablending($im, false);
 		imagesavealpha($im, true);
 		
 		$cropped = imagecropauto($im, IMG_CROP_SIDES);
+		imagedestroy($im);
 		if($cropped === false){
 			return;
 		}
-		imagedestroy($im);
 		imagealphablending($cropped, false);
 		imagesavealpha($cropped, true);
 		if($config["carpic"]["flip"] == true){
