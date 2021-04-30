b05b103ee084c857cc1487846bb109cc5dff6f28
Tim (robske_110) <robske110@users.noreply.github.com>
2021-04-30 22:50:42 +0200

    frontend: add meta mobile-web-app-capable=yes
    Adds support for home screen apps to launch in full screen on android

--- public/idView/idView.php
+++ public/idView/idView.php
@@ -22,6 +22,7 @@ spl_autoload_register(function ($class){
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, viewport-fit=cover">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	<meta name="mobile-web-app-capable" content="yes">
 	<title>IDView</title>
 </head>
 <body>
