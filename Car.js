robske110/IDDataLogger

commit 1041bbee07cda4072613297495dfe0c3c353d7cd

widget: Fix carPicture being the same w/ multiple Widgets and Cars
The same cache file for the carPicture was being overwritten by both
widgets.

Car.js
@@ -122,7 +122,9 @@ async function createWidget() {
 
 	carColumn.addSpacer(5)
 
-	const carImage = await getImage("car.png", baseURL+"/carPicture.php?key="+apiKey)
+	const carImage = await getImage(
+		baseURL.substr(baseURL.indexOf("://")+3).replace("/", "_")+"-car.png",
+		baseURL+"/carPicture.php?key="+apiKey)
 	let carImageElement = carColumn.addImage(carImage)
 
 	//carColumn.addSpacer(5)

commit 32225ed2b1b87e144b25bf15cba377c7bda0f3f7

widget: fix crashing when baseURl contains a path.
Bug introduced by commit 1041bbe, which uses the url in the filename to
be able to have multiple widgets for different Cars.

Car.js
@@ -123,7 +123,7 @@ async function createWidget() {
 	carColumn.addSpacer(5)
 
 	const carImage = await getImage(
-		baseURL.substr(baseURL.indexOf("://")+3).replace("/", "_")+"-car.png",
+		baseURL.substr(baseURL.indexOf("://")+3).replaceAll("/", "_")+"-car.png",
 		baseURL+"/carPicture.php?key="+apiKey)
 	carColumn.addImage(carImage)
 